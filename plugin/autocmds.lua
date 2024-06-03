01dc63c342ed9e47775a6bf641b20494b82004bb
Alchr Dev <hola.alchr@gmail.com>
2024-06-03 02:11:49 -0500

    feat(spell): add support for spell checking

--- plugin/autocmds.lua
+++ plugin/autocmds.lua
@@ -16,6 +16,18 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
 })
 
+-- Enable spell checking for certain file types
+api.nvim_create_autocmd(
+  { "BufRead", "BufNewFile" },
+  {
+    pattern = { "*.txt", "*.md", "*.tex" },
+    callback = function()
+      vim.opt.spell = true
+      vim.opt.spelllang = "es"
+    end,
+  }
+)
+
 -- Check if we need to reload the file when it changed
 vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, { command = "checktime" })
 
