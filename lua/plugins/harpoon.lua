452a621dc210d43d70b65be33c95c64c01696ba2
Alchr Dev <hola.alchr@gmail.com>
2024-10-06 23:49:04 -0500

    chore: update harpoon settings to version 2

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,26 +1,15 @@
 return {
   'ThePrimeagen/harpoon',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-  },
   config = function()
-    require('harpoon').setup {
-      tabline = false,
-      tabline_prefix = " ",
-      tabline_suffix = "  ",
-      menu = {
-        width = 80,
-      },
-    }
+    local mark = require('harpoon.mark')
+    local ui = require('harpoon.ui')
 
-    vim.keymap.set('n', '<leader>ho', '<cmd>lua require("harpoon.ui").toggle_quick_menu()<cr>', { desc = 'Toggle' })
-    vim.keymap.set('n', '<leader>ha', '<cmd>lua require("harpoon.mark").add_file()<cr>', { desc = 'Add file' })
+    vim.keymap.set('n', '<leader>ha', mark.add_file, { desc = 'Harpoon: Mark File'})
+    vim.keymap.set('n', '<leader>ho', ui.toggle_quick_menu, { desc = 'Harpoon: Toggle Menu'})
 
-    vim.keymap.set('n', '<leader>1', '<cmd>lua require("harpoon.ui").nav_file(1)<cr>', { desc = 'Go to file 1' })
-    vim.keymap.set('n', '<leader>2', '<cmd>lua require("harpoon.ui").nav_file(2)<cr>', { desc = 'Go to file 2' })
-    vim.keymap.set('n', '<leader>3', '<cmd>lua require("harpoon.ui").nav_file(3)<cr>', { desc = 'Go to file 3' })
-    vim.keymap.set('n', '<leader>4', '<cmd>lua require("harpoon.ui").nav_file(4)<cr>', { desc = 'Go to file 4' })
-    vim.keymap.set('n', '<leader>5', '<cmd>lua require("harpoon.ui").nav_file(5)<cr>', { desc = 'Go to file 5' })
-
+    vim.keymap.set('n', '<leader>1', function() ui.nav_file(1) end, { desc = 'Harpoon: File 1'})
+    vim.keymap.set('n', '<leader>2', function() ui.nav_file(2) end, { desc = 'Harpoon: File 2'})
+    vim.keymap.set('n', '<leader>3', function() ui.nav_file(3) end, { desc = 'Harpoon: File 3'})
+    vim.keymap.set('n', '<leader>4', function() ui.nav_file(4) end, { desc = 'Harpoon: File 4'})
   end
 }
