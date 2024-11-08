53a32061d1ce947415523c65419b54cc14339f09
Alchr Dev <hola.alchr@gmail.com>
2024-11-07 23:24:21 -0500

    style: add twilight and nightfall color scheme variants
    - Added 'twilight' (solid) and 'nightfall' (transparent) variants to the color scheme.
    - Adjusted highlight groups to support transparency:
      - `Telescope`, `MiniStatusline`, and other groups now use `bg = 'NONE'` or specific colors.

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -22,12 +22,14 @@ return {
 
       require('rose-pine').setup({
         styles = {
-          transparency = false,
+          transparency = true,
         },
         palette = {
           main = {
-            base = '#141420',
-            surface = '#141420',
+            -- base = '#141420', --twilight
+            -- surface = '#141420',--twilight
+            base = '#000000', --nightfall
+            surface = '#000000', --nightfall
           },
         },
 
@@ -35,45 +37,50 @@ return {
           Pmenu =  { bg = 'NONE' },
           PmenuThumb = { bg = 'NONE' },
           PmenuSbar = { bg = 'NONE'},
-          HarpoonBorder = { fg = '#524360' },
-          TelescopePromptPrefix = { fg = '#524360' },
-          TelescopeSelectionCaret = { bg = '#4d4763', fg = '#4d4763' },
-          TelescopePromptCounter = { fg = '#524360' },
-          VertSplit = { fg = '#2a2734' },
-          WinSeparator = { fg = '#2a2734' },
+          PmenuSel = { bg = '#261f2d' },
+          HarpoonBorder = { fg = '#463e59' },
 
           -- Highlight groups adjusted for different transparency settings.
           -- When transparency is disabled, both foreground and background colors are applied.
-
-          RenderMarkdownCode = { bg = '#261f2d' },
-          RenderMarkdownCodeInline = { bg = '#261f2d' },
-          PmenuSel = { bg = '#261f2d' },
-          TelescopeSelection  = { bg = '#261f2d' },
-          TelescopePromptNormal = { bg = '#1a1824' },
-          TelescopeResultsNormal = { bg = '#1a1824' },
-          TelescopePreviewNormal = { bg = '#1a1824' },
-          TelescopeBorder = { bg = '#443c63' },
-          TelescopeResultsBorder = { bg = '#1a1824', fg = '#1a1824' },
-          TelescopePreviewBorder = { bg = '#1a1824', fg = '#1a1824' },
-          TelescopePromptBorder = { bg = '#1a1824', fg = '#1a1824' },
-          TelescopePromptTitle = { fg = '#1a1824', bg = '#1a1824' },
-          TelescopeResultsTitle = { fg = '#1a1824', bg = '#1a1824' },
-          TelescopePreviewTitle = { fg = '#1a1824', bg = '#1a1824' },
-          MiniStatuslineFilename = { bg = '#1a1824' },
-          MiniStatuslineDevinfo = { bg = '#1a1824'},
+          -- VertSplit = { fg = '#2a2734' },
+          -- WinSeparator = { fg = '#2a2734' },
+          -- RenderMarkdownDash = { fg = '#261f2d'},
+          -- RenderMarkdownCode = { bg = '#261f2d' },
+          -- RenderMarkdownCodeInline = { bg = '#261f2d' },
+          -- TelescopeSelection  = { bg = '#261f2d' },
+          -- TelescopePromptNormal = { bg = '#1a1824' },
+          -- TelescopeResultsNormal = { bg = '#1a1824' },
+          -- TelescopePreviewNormal = { bg = '#1a1824' },
+          -- TelescopeBorder = { bg = '#443c63' },
+          -- TelescopeResultsBorder = { bg = '#1a1824', fg = '#1a1824' },
+          -- TelescopePreviewBorder = { bg = '#1a1824', fg = '#1a1824' },
+          -- TelescopePromptBorder = { bg = '#1a1824', fg = '#1a1824' },
+          -- TelescopePromptTitle = { fg = '#1a1824', bg = '#1a1824' },
+          -- TelescopeResultsTitle = { fg = '#1a1824', bg = '#1a1824' },
+          -- TelescopePreviewTitle = { fg = '#1a1824', bg = '#1a1824' },
+          -- TelescopePromptPrefix = { fg = '#524360' },
+          -- TelescopeSelectionCaret = { bg = '#4d4763', fg = '#4d4763' },
+          -- TelescopePromptCounter = { fg = '#524360' },
+          -- MiniStatuslineFilename = { bg = '#1a1824' },
+          -- MiniStatuslineDevinfo = { bg = '#1a1824'},
 
           -- When transparency is enabled, only foreground colors are applied. 
-          -- PmenuSel = { bg = '#373148' },
-          -- TelescopeBorder = { fg = '#373148' },
-          -- TelescopeSelection  = { bg = '#373148' },
-          -- TelescopePreviewTitle = { fg = '#524360' },
-          -- TelescopePromptTitle = { fg = '#524360' },
-          -- TelescopeResultsTitle = { fg = '#524360' },
-          -- RenderMarkdownCode = { bg = '#3a3240' },
-          -- RenderMarkdownCodeInline = { bg = '#3a3240' },
-          -- MiniStatuslineFilename = { bg= 'NONE' },
-          -- MiniStatuslineFileinfo = { bg = 'NONE' },
-          -- MiniStatuslineMode = { bg = 'NONE' },
+          TelescopeBorder = { fg = '#121212' },
+          TelescopeSelection  = { bg = '#261f2d' },
+          TelescopePreviewTitle = { fg = '#463e59' },
+          TelescopePromptTitle = { fg = '#463e59' },
+          TelescopeResultsTitle = { fg = '#463e59' },
+          TelescopePromptPrefix = { fg = '#463e59' },
+          TelescopeSelectionCaret = { bg = '#4d4763', fg = '#4d4763' },
+          TelescopePromptCounter = { fg = '#524360' },
+          RenderMarkdownCode = { bg = '#0d0d0d' },
+          RenderMarkdownCodeInline = { bg = '#0d0d0d' },
+          RenderMarkdownDash = { fg = '#121212'},
+          MiniStatuslineFilename = { bg= 'NONE' },
+          MiniStatuslineFileinfo = { bg = 'NONE' },
+          MiniStatuslineMode = { bg = 'NONE' },
+          VertSplit = { fg = '#121212' },
+          WinSeparator = { fg = '#121212' },
         },
       })
 
