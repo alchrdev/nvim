b1e10b1f36d695adfec9ab32f0ff6dce85309d0e
Alchr Dev <hola.alchr@gmail.com>
2024-11-21 10:49:32 -0500

    feat(lsp): update configuration for tsserver and mason
    - Configure tsserver settings for JavScript and TypeScript, including inlay hints.
    - Add root patterns for tsserver to enhance project detection.
    - Update Mason's ensure_installed list with relevant tools (e.g.,
      stylua, eslint_d).

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -24,7 +24,38 @@ return {
         html = {},
         tailwindcss = {},
         tsserver = {
-          root_dir = require('lspconfig').util.root_pattern('.git'),
+          root_dir = require('lspconfig').util.root_pattern(
+            -- 'pnpm-workspace.yaml',
+            -- 'yarn.lock',
+            -- 'pnpm-lock.yaml',
+            '.eslintrc.json',
+            '.eslintrc',
+            '.git'
+          ),
+          settings = {
+            javascript = {
+              inlayHints = {
+                includeInlayEnumMemberValueHints = false,
+                includeInlayFunctionLikeReturnTypeHints = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayParameterNameHints = 'all', -- 'none' | 'literals' | 'all';
+                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                includeInlayPropertyDeclarationTypeHints = false,
+                includeInlayVariableTypeHints = false,
+              },
+            },
+            typescript = {
+              inlayHints = {
+                includeInlayEnumMemberValueHints = false,
+                includeInlayFunctionLikeReturnTypeHints = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayParameterNameHints = 'all', -- 'none' | 'literals' | 'all';
+                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                includeInlayPropertyDeclarationTypeHints = false,
+                includeInlayVariableTypeHints = false,
+              },
+            },
+          },
         },
         lua_ls = {
           settings = {
@@ -46,18 +77,39 @@ return {
         },
       }
       require('mason').setup()
-      local ensure_installed = vim.tbl_keys(servers or {})
+
+      -- You can add other tools here that you want Mason to install for you, so that they are available from within Neovim.
+      local ensure_installed = vim.tbl_map(function(key)
+        return key == 'tsserver' and 'ts_ls' or key
+      end, vim.tbl_keys(servers or {}))
       vim.list_extend(ensure_installed, {
-        -- 'prettierd',
-        -- 'eslint_d',
-        'stylua',
+        -- 'goimports', -- Format imports in Go (gopls includes gofmt already)
+        -- 'prettierd', -- Used to format JavaScript, TypeScript, HTML, JSON, etc.
+        -- 'eslint_d', -- Used to lint and fix JavaScript, TypeScript, and other web-related code efficiently
+        'stylua', -- Used to format Lua code
+        -- 'ktlint', -- Used to format (and lint) Kotlin code
       })
-      require('mason-tool-installer').setup({ ensure_installed = ensure_installed })
+      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+
       require('mason-lspconfig').setup({
         handlers = {
           function(server_name)
+            server_name = server_name == 'tsserver' and 'ts_ls' or server_name
             local server = servers[server_name] or {}
+            -- This handles overriding only values explicitly passed
+            -- by the server configuration above. Useful when disabling
+            -- certain features of an LSP (for example, turning off formatting for tsserver)
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+
+            -- FIXME: workaround for https://github.com/neovim/neovim/issues/28058
+            for _, v in pairs(server) do
+              if type(v) == 'table' and v.workspace then
+                v.workspace.didChangeWatchedFiles = {
+                  dynamicRegistration = false,
+                  relativePatternSupport = false,
+                }
+              end
+            end
             require('lspconfig')[server_name].setup(server)
           end,
         },
