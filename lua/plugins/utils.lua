8b17367fffcee4b83198e757ebf9a5ece1c38f99
Alchr Dev <hola.alchr@gmail.com>
2024-12-08 21:39:52 -0500

    feat(utils): add ccc.nvim and flash.nvim plugins
    - Added ccc.nvim for inline color display.
    - Added flash.nvim for enhanced navigation and Treesitter integration.

---
+++ lua/plugins/utils.lua
@@ -0,0 +1,69 @@
+return {
+  -- Shows color inline for the various colour codes
+  {
+    'uga-rosa/ccc.nvim',
+    config = function()
+      require('ccc').setup {
+        highlighter = {
+          auto_enable = true,
+          lsp = false, -- i think doing this would enable tailwindcss colorizer, which is buggy
+          filetypes = {
+            'css',
+            'typescript',
+            'typescriptreact',
+            'html',
+          },
+        },
+      }
+    end,
+  },
+  -- Enhances navigation with search labels, precise motions, and Treesitter support
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {
+      jump = {
+        autojump = true,
+      },
+      modes = {
+        char = {
+          jump_labels = true,
+          multi_line = false,
+        }
+      }
+    },
+    -- stylua: ignore
+    keys = {
+      { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+      { "S",     mode = { "n" },           function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+      { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+      { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+    },
+  },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {
+      jump = {
+        autojump = true,
+      },
+      modes = {
+        char = {
+          jump_labels = true,
+          multi_line = false,
+        }
+      }
+    },
+    -- stylua: ignore
+    keys = {
+      { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+      { "S",     mode = { "n" },           function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+      { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+      { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+    },
+  },
+}
