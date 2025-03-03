4d250092bb6535b6674c3a5c94c00306006d377f
Alchr Dev <hola.alchr@gmail.com>
2025-03-03 03:04:54 -0500

    feat(git): update icons and key mappings for gitsigns and vim-fugitive
    Replaced default gitsigns symbols with custom icons from config.
    Adjusted key mappings for navigating hunks and git actions.
    Added new mappings for git commit, push, pull, and log tree.

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -3,13 +3,25 @@ return {
     'lewis6991/gitsigns.nvim',
     config = function()
       local gitsigns = require 'gitsigns'
+      local icons = require('config.icons')
+
       gitsigns.setup {
         signs = {
-          add = { text = '+' },
-          change = { text = '~' },
-          delete = { text = '_' },
-          topdelete = { text = '‾' },
-          changedelete = { text = '~' },
+          add = {
+            text = icons.ui.BoldLineLeft,
+          },
+          change = {
+            text = icons.ui.BoldLineLeft,
+          },
+          delete = {
+            text = icons.ui.Triangle,
+          },
+          topdelete = {
+            text = icons.ui.Triangle,
+          },
+          changedelete = {
+            text = icons.ui.BoldLineLeft,
+          },
         },
         signcolumn = false,
         numhl = false,
@@ -20,10 +32,10 @@ return {
           follow_files = true,
         },
         attach_to_untracked = true,
-        current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+        current_line_blame = false, -- Toggle with :Gitsigns toggle_current_line_blame
         current_line_blame_opts = {
           virt_text = true,
-          virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+          virt_text_pos = 'eol',
           delay = 1000,
           ignore_whitespace = false,
         },
@@ -42,13 +54,15 @@ return {
 
         on_attach = function (bufnr)
           vim.keymap.set('n', '<leader>H', require("gitsigns").preview_hunk,
-            { buffer = bufnr, desc = 'Preview git hunk '})
+            { buffer = bufnr, desc = 'Preview git hunk' })
 
-          vim.keymap.set('n', ']]', require("gitsigns").next_hunk,
-            { buffer = bufnr, desc = 'Next git hunk '})
+          vim.keymap.set('n', ']]', function()
+            require('gitsigns').nav_hunk('next')
+          end, { buffer = bufnr, desc = '[N]ext git hunk' })
 
-          vim.keymap.set('n', '[[', require("gitsigns").prev_hunk,
-            { buffer = bufnr, desc = 'Previous git hunk '})
+          vim.keymap.set('n', '[[', function()
+            require('gitsigns').nav_hunk('prev')
+          end, { buffer = bufnr, desc = '[P]revious git hunk' })
         end
       }
       vim.keymap.set(
@@ -66,8 +80,8 @@ return {
       vim.keymap.set(
         'n',
         '<leader>gh',
-        ':Gitsigns stage_hunk<cr>',
-        { noremap = true, silent = true, desc = 'Stage [h]unk' }
+        ':Gitsigns reset_hunk<cr>',
+        { noremap = true, silent = true, desc = '[R]eset hunk' }
       )
     end,
   },
@@ -75,8 +89,12 @@ return {
   {
     'tpope/vim-fugitive',
     config = function()
-      vim.keymap.set('n', '<leader>go', ':Git<cr>', { noremap = true, silent = true, desc = '[o]pen' })
-      vim.keymap.set('n', '<leader>gb', ':Git blame<cr>', { noremap = true, silent = true, desc = '[b]lame' })
+      vim.keymap.set('n', '<leader>go', ':Git<cr>', { noremap = true, silent = true, desc = '[O]pen' })
+      vim.keymap.set('n', '<leader>gB', ':Git blame<cr>', { noremap = true, silent = true, desc = '[B]lame' })
+      vim.keymap.set('n', '<leader>gc', ':Git commit<CR>', { noremap = true, silent = true, desc = '[G]it commit' })
+      vim.keymap.set('n', '<leader>gp', ':Git push<CR>', { noremap = true, silent = true, desc = '[G]it push' })
+      vim.keymap.set('n', '<leader>gP', ':Git pull<CR>', { noremap = true, silent = true, desc = '[G]it pull' })
+      vim.keymap.set('n', '<leader>ga', ':Git log --oneline --graph --decorate --all<CR>', { noremap = true, silent = true, desc = '[G]it log tree' })
     end,
   },
 }
