2cf1653c87bfe591f655c5d81a8b4dc620ee1f93
Alchr Dev <hola.alchr@gmail.com>
2025-06-02 18:22:00 -0500

    refactor(plugins): replace telescope with explorer module and relocate dependency
    Removed Telescope configuration (telescope.lua) and replaced it with the Explorer module from Snacks.nvim. Moved the 'nvim-lua/plenary.nvim' dependency from telescope.lua to harpoon.lua for better modularization and dependency management.

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,5 +1,8 @@
 return {
   'ThePrimeagen/harpoon',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
   config = function()
     local mark = require('harpoon.mark')
     local ui = require('harpoon.ui')
